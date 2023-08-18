4dee71d5860350d914386d892b4692ebc468c6d0
Cameron Lee <cameronslee@hotmail.com>
2023-08-18 15:20:45 -0700

    big ol refactor, one page wonder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,130 +1,79 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import GeorgeImage from "public/george.png";
-import { Terminal } from 'primereact/terminal';
+import { Client, IPinnedItem } from "../components/gh-pins"
 
-import { TerminalService } from 'primereact/terminalservice';
-import { useRouter } from 'next/router';
-import {useEffect, useState } from 'react'; 
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
-const TerminalDemo = () => {
-  const router = useRouter();
+import { env } from "~/env.mjs";
 
-  const [georgeUp, setGeorgeUp] = useState(false);
-  const [help, setHelp] = useState(false);
-
-  const commandHandler = (text: string) => {
-    let response;
-    const argsIndex = text.indexOf(' ');
-    const command = argsIndex !== -1 ? text.substring(0, argsIndex) : text;
-
-    switch (command) {
-      case 'date':
-        response = 'Today is ' + new Date().toDateString();
-        break;
-
-      case 'clear':
-        response = null;
-        break;
-
-      case 'shambles':
-        setGeorgeUp(!georgeUp);
-        console.log(georgeUp==true ? "George is up" : "George is down");
-        response = <p>George is a good little monkey <br/> and always very curious.</p>;
-        break;
-
-      case 'help':
-      case '-h': 
-      case 'h':
-      case '-help': 
-      case '--help': 
-        response = 
-              <p>
-                Commands: <br />
-                <span className="text-green-400">about</span> - who am i?<br />
-                <span className="text-green-400">projects</span> - view list of projects<br />
-                <span className="text-green-400">clear</span> - clear output<br />
-              </p>;
-        break;
-
-      case 'about':
-        response = 
-              <div>
-              <br />
-                <span className="text-green-400">Cameron Lee</span>
-                <p>(Programmer / Scientist)</p> <br />
-
-                <span className="text-green-400">About: </span>
-                <p> Curious and passionate about technology <br/> with a strong foundation in Computer Science</p> <br />
-
-                <span className="text-green-400">Links: </span> <br />
-                <Link href={"https://github.com/cameronslee"} target="_blank" className="underline">Github</Link><br />
-                <Link href={"https://www.linkedin.com/in/cameronseilee/"} target="_blank" className="underline">LinkedIn</Link><br /><br />
-                <span className="text-green-400">Education: </span>
-                &nbsp;&nbsp;<p>Computer Science B.S - Seattle University</p> <br />
-              </div>;
-        break;
-
-      case 'projects':
-        response = "To projects";
-        router.push('/projects').catch(err => console.log(err));
-        break;
-        
-
-      default:
-        response = 'Unknown command: ' + command + '. Type "help" for a list of commands.';
-        break;
-    }
-
-    if (response) {
-      TerminalService.emit('response', response);
-    }
-    else {
-      TerminalService.emit('clear');
-    }
-  }
-
-  useEffect(() => {
-      TerminalService.on('command', commandHandler);
-
-      return () => {
-      TerminalService.off('command', commandHandler);
-      }
-      });
-
-  return (
-      <div className="bg-slate-950 text-white p-4 rounded-2xl font-mono">
-      {georgeUp && <img src={GeorgeImage.src} alt={"CG"} className="bg-transparent w-32" />}
-      <Terminal 
-      className="[&_.p-terminal-input]:bg-transparent
-      [&_.p-terminal-input]:outline-none
-      [&_.p-terminal-content]:break-words
-      " prompt="cameron@entity $"/>
-      </div>
-      );
+export const getServerSideProps: GetServerSideProps<{
+  repo: IPinnedItem[];
+}> = async () => {
+  Client.init(env.GITHUB_ACCESS_TOKEN);
+  const repo : IPinnedItem[] = await Client.getPins().then((res: IPinnedItem[]) => {
+    return res;
+  });
+  return { props: { repo } }
 }
 
 
-export default function Home() {
+const RepoCard = (repo: IPinnedItem) => {
+  console.log(repo.name)
+  return (
+    <div>
+      <Link target='_blank' href={repo.homepageUrl ? repo.homepageUrl : repo.url}>
+        <div className="flex flex-col items-center justify-center gap-4 m-2.5 font-mono">
+          <div className="w-full max-w-sm overflow-hidden rounded-lg bg-zinc-800 shadow-md duration-300 hover:scale-105 hover:shadow-xl p-4 space-x-4 space-y-4">
+            <h1 className="font-extrabold font-mono tracking-tight text-white text-sm">
+              <Link target='_blank' className="hover:text-green-400" href={
+                repo.homepageUrl ? repo.homepageUrl : repo.url
+              }>{repo.name}</Link>
+            </h1>
+            <div className="text-white bg-gradient-to-b border-white text-sm">
+              {repo.description}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+
+export default function Home({
+  repo,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(repo)
   return (
     <>
       <Head>
         <title>cameron</title>
         <meta name="description" content="foo" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold font-mono tracking-tight text-white sm:text-[5rem]">
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center text-white gap-12 font-mono px-4 py-16 ">
+          <h1 className="text-5xl tracking-tight sm:text-[5rem]">
             cameron
           </h1>
-          <div className="text-white bg-gradient-to-b border-white">
-          
-          <TerminalDemo />
-          </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <div className="flex flex-col items-center justify-center gap-4">
-            </div>
+          <div>
+            <br />
+            <span className="text-green-400">Cameron Lee</span>
+            <p>(Programmer / Scientist)</p> <br />
+
+            <span className="text-green-400">About: </span>
+            <p> Curious and passionate about technology <br /> with a strong foundation in Computer Science</p> <br />
+            <p> Interested in web technologies and  <br/> deep learning</p> <br />
+
+            <span className="text-green-400">Links: </span> <br />
+            <Link href={"https://github.com/cameronslee"} target="_blank" className="underline">Github</Link><br />
+            <Link href={"https://www.linkedin.com/in/cameronseilee/"} target="_blank" className="underline">LinkedIn</Link><br /><br />
+            <span className="text-green-400">Education: </span>
+            &nbsp;&nbsp;<p>Computer Science B.S - Seattle University</p> <br />
+            <span className="text-green-400">Notable Projects: </span>
+            {repo.map((repo) => (
+              <RepoCard {...repo} key={repo.id} />
+            ))}
           </div>
         </div>
       </main>
