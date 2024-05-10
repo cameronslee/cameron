f691ea3d617e78fd3c6cf4c9066a23e764a6dbc0
cameron <cameronslee@hotmail.com>
2024-05-10 10:58:43 -0700

    will this break prod lets find out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,6 @@ export const getServerSideProps: GetServerSideProps<{
   return { props: { repo } }
 }
 
-
 const RepoCard = (repo: IPinnedItem) => {
   console.log(repo.name)
   return (
@@ -26,11 +25,11 @@ const RepoCard = (repo: IPinnedItem) => {
         <div className="flex flex-col items-center justify-center gap-4 m-2.5 font-mono">
           <div className="w-full max-w-sm overflow-hidden rounded-lg bg-zinc-800 shadow-md duration-300 hover:scale-105 hover:shadow-xl p-4 space-x-4 space-y-4">
             <h1 className="font-extrabold font-mono tracking-tight text-white text-sm">
-              <Link target='_blank' className="hover:text-fuchsia-500" href={
+              <Link target='_blank' className="hover:text-emerald-400" href={
                 repo.homepageUrl ? repo.homepageUrl : repo.url
               }>{repo.name}</Link>
             </h1>
-            <div className="text-white bg-gradient-to-b border-white text-sm">
+            <div className="text-indigo-700 bg-gradient-to-b border-white text-sm">
               {repo.description}
             </div>
           </div>
@@ -39,7 +38,6 @@ const RepoCard = (repo: IPinnedItem) => {
     </div>
   )
 }
-
 
 export default function Home({
   repo,
@@ -58,24 +56,16 @@ export default function Home({
           </h1>
           <div>
             <br />
-            <span className="text-sky-600">Cameron Lee</span>
+            <span className="text-violet-400">Cameron Lee</span>
             <p>(Computer Programmer)</p> <br />
 
-            <span className="text-sky-600">About: </span>
-            <p> 
-            - Curious and Dilligent
-            <br/>
-            - Interested in compilers and operating systems
-            </p>
-            <br />
-
-            <span className="text-sky-600">Links: </span> <br />
+            <span className="text-violet-400">Links: </span> <br />
             <Link href={"https://cameronslee.github.io/blog/"} target="_blank" className="underline">Blog</Link><br />
             <Link href={"https://github.com/cameronslee"} target="_blank" className="underline">Github</Link><br />
             <Link href={"https://www.linkedin.com/in/cameronseilee/"} target="_blank" className="underline">LinkedIn</Link><br /><br />
-            <span className="text-sky-600">Education: </span>
+            <span className="text-violet-400">Education: </span>
             &nbsp;&nbsp;<p>Computer Science B.S - Seattle University</p> <br />
-            <span className="text-sky-600">Notable Projects: </span>
+            <span className="text-violet-400">Notable Projects: </span>
             {repo.map((repo) => (
               <RepoCard {...repo} key={repo.id} />
             ))}
