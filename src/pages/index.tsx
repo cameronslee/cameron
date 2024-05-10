41c6f5f5ed1bee2ae1c5631f743019c814453def
cameron <cameronslee@hotmail.com>
2024-05-10 11:19:21 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,11 +25,11 @@ const RepoCard = (repo: IPinnedItem) => {
         <div className="flex flex-col items-center justify-center gap-4 m-2.5 font-mono">
           <div className="w-full max-w-sm overflow-hidden rounded-lg bg-zinc-800 shadow-md duration-300 hover:scale-105 hover:shadow-xl p-4 space-x-4 space-y-4">
             <h1 className="font-extrabold font-mono tracking-tight text-white text-sm">
-              <Link target='_blank' className="text-emerald-400 hover:text-rose-400" href={
+              <Link target='_blank' className="text- hover:text-rose-400" href={
                 repo.homepageUrl ? repo.homepageUrl : repo.url
               }>{repo.name}</Link>
             </h1>
-            <div className="text-indigo-700 bg-gradient-to-b border-white text-sm">
+            <div className="text-white bg-gradient-to-b border-white text-sm">
               {repo.description}
             </div>
           </div>
@@ -57,14 +57,14 @@ export default function Home({
           <div>
             <br />
             <span className="text-violet-400">Cameron Lee</span>
-            <p className="text-emerald-400">(Computer Programmer)</p> <br />
+            <p className="text-white">(Computer Programmer)</p> <br />
 
             <span className="text-violet-400">Links: </span> <br />
-            <Link href={"https://cameronslee.github.io/blog/"} target="_blank" className="text-emerald-400 hover:text-rose-400 underline">Blog</Link><br />
-            <Link href={"https://github.com/cameronslee"} target="_blank" className="text-emerald-400 hover:text-rose-400 underline">Github</Link><br />
-            <Link href={"https://www.linkedin.com/in/cameronseilee/"} target="_blank" className="text-emerald-400 hover:text-rose-400 underline">LinkedIn</Link><br /><br />
+            <Link href={"https://cameronslee.github.io/blog/"} target="_blank" className="text-white hover:text-rose-400 hover:underline">Blog</Link><br />
+            <Link href={"https://github.com/cameronslee"} target="_blank" className="text-white hover:text-rose-400 hover:underline">Github</Link><br />
+            <Link href={"https://www.linkedin.com/in/cameronseilee/"} target="_blank" className="text-white hover:text-rose-400 hover:underline">LinkedIn</Link><br /><br />
             <span className="text-violet-400">Education: </span>
-            &nbsp;&nbsp;<p className="text-emerald-400 hover:text-rose-400">Computer Science B.S - Seattle University</p> <br />
+            &nbsp;&nbsp;<p className="text-white hover:text-rose-400">Computer Science B.S - Seattle University</p> <br />
             <span className="text-violet-400">Notable Projects: </span>
             {repo.map((repo) => (
               <RepoCard {...repo} key={repo.id} />
